1c685a9d02af1a648eab1e9bedc874b41a50d226
João Messias <joao.2020317758@aluno.iffar.edu.br>
2025-10-24 00:27:16 -0300

    Inicio da criação do cabeçalho

---
+++ fctec/src/Elementos/cabecalho/index.js
@@ -0,0 +1,10 @@
+import '../cabecalho/cabecalho.css'
+
+export default function Cabecalho(){
+    return(
+        <div className='cabecalho'>
+            <button className='cabecalho_botao'>Menu</button>
+            <img className='foto_cabecalho'src="/assets/imagens/Logo Horizontal - Branca.svg" alt="Logo BioDelivery" />
+        </div>
+    )   
+}
